376754eefed2ce9fe59bb012e5686811e4c661b2
victorbobkov <victorbobkov@gmail.com>
2022-10-28 01:00:25 +0400

    Add getting data from array

--- index.js
+++ index.js
@@ -2,7 +2,36 @@ new Vue({
   el: '.app',
   data () {
     return {
-      showCategoryPopup: false
-    }
-  }
+      searchQuery: null,
+      showCategoryPopup: false,
+      categories: [
+        {name: 'Нутра'},
+        {name: 'Крипта'},
+        {name: 'Товарка'},
+        {name: 'Нутра 2'},
+        {name: 'Крипта 2'},
+        {name: 'Товарка 2'},
+        {name: 'Нутра 3'},
+        {name: 'Крипта 3'},
+        {name: 'Товарка 3'},
+        {name: 'Нутра 4'},
+        {name: 'Крипта 4'},
+        {name: 'Товарка 4'},
+      ]
+    };
+  },
+  // computed: {
+  //   resultQuery() {
+  //     if (this.searchQuery) {
+  //       return this.categories.filter((category) => {
+  //         return this.searchQuery
+  //           .toLowerCase()
+  //           .split(' ')
+  //           .every(v => category.title.toLowerCase().includes(v))
+  //       })
+  //     } else {
+  //       return this.categories
+  //     }
+  //   }
+  // },
 });
